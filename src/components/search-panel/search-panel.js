4e105609c1028aba01266ebf9a2afff3feb12dd4
Vano-Stas <st.ivan727@gmail.com>
2019-06-20 21:31:09 +0300

    upload files

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+import "./search-panel.css";
+
+export default class SearchPanel extends Component {
+
+    state = {
+        term: ""
+    };
+
+    onSearchChange = (e) => {
+        const term = e.target.value;
+        this.setState({ term });
+        this.props.onSearchChange(term);
+    };
+
+    render () {
+        return (
+            <input type="text"
+                    placeholder="search"
+                    className="form-control search-input"
+                    value = {this.state.term}
+                    onChange = {this.onSearchChange} />
+        );
+    };
+};
