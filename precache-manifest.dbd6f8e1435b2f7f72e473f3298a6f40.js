dd9b609caaef80258efcb471a6dd22aec0962da6
Vano-Stas <st.ivan727@gmail.com>
2019-06-20 21:38:13 +0300

    Updates

---
+++ precache-manifest.dbd6f8e1435b2f7f72e473f3298a6f40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8f3fe904ea967a4fb9a9f32b58ff52b",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "166a061170966dced089",
+    "url": "/todo-react/static/css/main.a0ca11b7.chunk.css"
+  },
+  {
+    "revision": "06f44d7c6eeee966ea35",
+    "url": "/todo-react/static/js/2.34ad89f7.chunk.js"
+  },
+  {
+    "revision": "166a061170966dced089",
+    "url": "/todo-react/static/js/main.85d7a271.chunk.js"
+  },
+  {
+    "revision": "cd203911fc85a7f73b39",
+    "url": "/todo-react/static/js/runtime~main.6292bb99.js"
+  }
+]);
